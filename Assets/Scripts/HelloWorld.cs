037910e19e942e94dbfd687dc3ba701a1e698ce8
Francis Ransom <frankieransom@hotmail.com>
2025-05-16 19:26:18 -0400

    Intialized Unity Project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Here we go");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Let's begin");
+    }
+}
